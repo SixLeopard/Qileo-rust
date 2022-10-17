08ea6dff65a2e6a387a8dacd6899a57b5b6ef18a
SixLeopard <SixLeopard@outlook.com.au>
2022-10-17 15:29:25 +1000

    learning more rust (guessin game)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
 #![allow(nonstandard_style)]
 
+use std::io;
+use rand::Rng;
+
 fn main() {
-    for _x in 1..100 {
-        println!("Hello, world!");
+    loop{
+        let mut playAgain = String::new();
+
+        io::stdin().read_line(&mut playAgain).expect("Failed to read input");
+
+        let playAgain: bool = playAgain.trim().parse().expect("not bool");
+
+        if playAgain{
+            GameLoop();
+        }else{
+            break;
+        }
+    }
+    
+}
+
+fn GameLoop(){
+    let number = rand::thread_rng().gen_range(1..100);
+    loop {
+        println!("Guess a Number");
+
+        println!("Please Input your Guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        if number == guess {
+            println!("Correct!");
+            break;
+        }
+
+        if number < guess{
+            println!("To High!");
+        }
+
+        if number > guess{
+            println!("To Low!");
+        }
     }
 }
